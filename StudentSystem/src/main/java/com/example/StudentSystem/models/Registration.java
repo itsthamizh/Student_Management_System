8ade9d75cc516dfb13a17e471a7d129e0ee28110
tamizh <tamizh@DESKTOP-AHSCTDR>
2021-05-09 01:32:12 +0530

    First commit

---
+++ StudentSystem/src/main/java/com/example/StudentSystem/models/Registration.java
@@ -0,0 +1,73 @@
+package com.example.StudentSystem.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private Integer regno;
+    private String name;
+    private String dept;
+    private Integer year;
+
+    public Registration(long id, Integer regno, String name, String dept, Integer year) {
+        this.id = id;
+        this.regno = regno;
+        this.name = name;
+        this.dept = dept;
+        this.year = year;
+    }
+
+    public Registration() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getRegno() {
+        return regno;
+    }
+
+    public void setRegno(Integer regno) {
+        this.regno = regno;
+    }
+
+}
